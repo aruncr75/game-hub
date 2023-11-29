ac459faa9819c61e47e3665a664cc5dbfd46cce2
aruncr75 <aruncr75@gmail.com>
2023-11-30 02:12:30 +0530

    Filter Games by Genre

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 import { NavBar } from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
+import { useState } from "react";
+import { Genre } from "./hooks/useGenres";
 
 function App() {
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+
   return (
     // <Grid templateAreas={`"nav nav" "aside main"`}>
     <Grid
@@ -12,19 +16,20 @@ function App() {
         lg: `"nav nav " "aside main"`, // ~992px
       }}
       templateColumns={{
-        base: '1fr',
-        lg: '250px 1fr'
+        base: "1fr",
+        lg: "250px 1fr",
       }}
     >
       <GridItem area="nav">
-      <NavBar />
+        <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" >
-        <GenreList />        </GridItem>
+        <GridItem area="aside">
+          <GenreList onSelectGenre={(genre) =>setSelectedGenre(genre)} />
+        </GridItem>
       </Show>
-      <GridItem area="main" >
-      <GameGrid></GameGrid>
+      <GridItem area="main">
+        <GameGrid selectedGenre={selectedGenre}></GameGrid>
       </GridItem>
     </Grid>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import useData from "./useData";
+import { Genre } from "./useGenres";
 export interface Platform{
     id:number;
     name:string;
@@ -17,8 +18,11 @@ export interface Games{
 }
 
  
-  const useGames = () => 
-    useData<Games>('/games')
+  const useGames = (selectedGenre:Genre | null) => 
+    useData<Games>('/games' ,{params:{
+        genres:selectedGenre?.id
+
+    }} ,[selectedGenre?.id])
 
  
 
