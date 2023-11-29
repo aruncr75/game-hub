26d2cfae82de89765ddb48bdb931c27085b7dc4f
aruncr75 <aruncr75@gmail.com>
2023-11-29 16:43:54 +0530

    Displaying PLatfroms Icon

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,21 @@
-import { SimpleGrid, Text } from '@chakra-ui/react'
-import useGames from '../hooks/useGames'
-import GameCard from './GameCard';
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
- const GameGrid = () => {
+const GameGrid = () => {
+  const { games, error } = useGames();
 
-  const {games ,error} = useGames();
+  return (
+    <>
+      {error && <Text> {error}</Text>}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 3 }} spacing={10}>
+        {" "}
+        {games.map((game) => (
+          <GameCard key={game.id} game={game}></GameCard>
+        ))}
+      </SimpleGrid>
+    </>
+  );
+};
 
-  return (<>
-  {error &&<Text> {error}</Text>}
-    <SimpleGrid columns={{sm:1 ,md:2 ,lg:3 ,xl:3}} spacing={10}>  {
-        games.map(game => <GameCard key={game.id} game={game}></GameCard>)}</SimpleGrid >
-        
-</>
-  )
-}
-
-export default GameGrid
+export default GameGrid;
