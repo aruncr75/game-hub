c4c8346291d1c4237ee6a979a519ebfcc02f28ca
aruncr75 <aruncr75@gmail.com>
2023-12-07 16:49:14 +0530

    Added React query devtools and converted genre with react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import theme from './theme';
+import theme from "./theme";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const query = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-     <ChakraProvider theme={theme}>
-     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={query}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+        <ReactQueryDevtools></ReactQueryDevtools>
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
