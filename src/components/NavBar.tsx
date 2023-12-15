6352279378fa6b9a107a657ae8918977993f095d
aruncr75 <aruncr75@gmail.com>
2023-12-15 22:11:00 +0530

    Fix the Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,18 @@
-import { Box, HStack } from '@chakra-ui/react'
-import { Image ,Text } from '@chakra-ui/react'
-import logo from '../assets/logo.webp';
-import { ColorModeSwitch } from './ColorModeSwitch';
-import SearchInput from './SearchInput';
-
-
+import { Box, HStack } from "@chakra-ui/react";
+import { Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import { ColorModeSwitch } from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-   <HStack justifyContent={'space-between'} padding={"10px"}> 
-    <Image src={logo} boxSize="60px" />
-    <SearchInput ></SearchInput>
-    <ColorModeSwitch></ColorModeSwitch>
-   </HStack>
-  )
-}
+    <HStack justifyContent={"space-between"} padding={"10px"}>
+      <Link to={"/"}>
+        <Image src={logo} boxSize="60px" objectFit='cover'/>
+      </Link>
+      <SearchInput></SearchInput>
+      <ColorModeSwitch></ColorModeSwitch>
+    </HStack>
+  );
+};
